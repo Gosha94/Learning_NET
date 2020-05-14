a549abcf8d0424d1893e4f996004ea6e2468a1f6
Gosha94 <gerizch@rambler.ru>
2020-05-14 13:04:02 +0300

    14_05 Добавлено приложение сортировки карт

---
+++ SortCardsSimulator/Classes/Card.cs
@@ -0,0 +1,23 @@
+﻿using SortCardsSimulator.Enums;
+using System.Collections.Generic;
+
+namespace SortCardsSimulator.Classes
+{
+    class Card
+    {
+        // Enum Suit (Spades, Clubs, Diamonds, Hearts)
+        public Suits Suit { get; set; }
+        // Enum Value(Ace, Two, Three…Ten, Jack, Queen, King), а также
+        public Values Value { get; set; }
+        // только для чтения поле Name(Ace of Spades (туз пик), Five of Diamonds(пятерка бубен)
+        public string Name { get { return Value.ToString() + " " + Suit.ToString(); } }
+        // поле указывающее на то прошла ли сортировка списка        
+
+        //public Card(Suits suit, Values value)
+        //{
+        //    this.Suit = suit;
+        //    this.Value = value;
+        //}
+    }
+
+}
--- SortCardsSimulator/Program.cs
+++ SortCardsSimulator/Program.cs
@@ -1,13 +1,43 @@
-﻿namespace SortCardsSimulator
+﻿using System;
+using SortCardsSimulator.Classes;
+using SortCardsSimulator.Enums;
+using System.Collections.Generic;
+
+namespace SortCardsSimulator
 {
+
     class Program
-    {
+    {       
+
         static void Main(string[] args)
         {
-            List<Card> cards = new List<Card>
-            {
+            Random random = new Random();
 
+            List<Card> cards = new List<Card> { // Generator of 5 random cards
+             new Card() { Suit = (Suits)random.Next(4), Value = (Values)random.Next(1, 14) },  // Next(int32) возвращает неотрицательное меньше указанного, значит от 0 до 3
+             new Card() { Suit = (Suits)random.Next(4), Value = (Values)random.Next(1, 14) },  // Next(int32, int32) возвращает от 0 до 13, верхний предел исключен, нижний включен
+             new Card() { Suit = (Suits)random.Next(4), Value = (Values)random.Next(1, 14) },
+             new Card() { Suit = (Suits)random.Next(4), Value = (Values)random.Next(1, 14) },
+             new Card() { Suit = (Suits)random.Next(4), Value = (Values)random.Next(1, 14) },
             };
+
+            Console.WriteLine("Пять случайных карт:");
+
+            PrintCards(cards);
+
+            CardComparerValueThenSuit cardComparer = new CardComparerValueThenSuit();
+            cards.Sort(cardComparer);
+            Console.WriteLine("Карты, отсортированные по увеличению крутости:");
+            PrintCards(cards);
+            Console.ReadKey();
         }
+
+        public static void PrintCards(List<Card> cards)
+        {                        
+            foreach (Card card in cards)
+                Console.WriteLine(card.Value.ToString() + " " + card.Suit.ToString());
+            Console.WriteLine("\n");
+        }       
+
     }
 }
