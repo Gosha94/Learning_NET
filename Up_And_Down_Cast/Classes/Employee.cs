161bfaca9928bf84ffa4b5fce029da817d821fa9
Gosha94 <gerizch@rambler.ru>
2021-04-08 23:17:04 +0300

    Gosha Add Up and Downcast trening

---
+++ Up_And_Down_Cast/Classes/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace Up_And_Down_Cast.Classes
+{
+    public class Employee
+    {
+        public string name;
+
+        public int GetYearsEmployed()
+        {
+            return 5;
+        }
+
+        public virtual string GetProgressReport()
+        {
+            return "Employee report";
+        }
+
+        public static Employee Lookup(string name)
+        {
+            return new Manager(name);
+        }
+    }
+}
